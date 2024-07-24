1a8e73a17c51188b58bb41365673e830aa16d6dc
reschmax <reschmax@msu.edu>
2024-07-24 13:34:27 -0400

    Established first simple html template

--- front-end-angular/src/app/app.component.ts
+++ front-end-angular/src/app/app.component.ts
@@ -5,8 +5,8 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: './home/home.component.html',
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'front-end-angular';
