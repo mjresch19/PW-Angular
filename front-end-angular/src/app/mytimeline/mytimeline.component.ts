20c2bbf9293d78ccf95ee7b250c5fa3c8769e237
reschmax <reschmax@msu.edu>
2024-07-25 13:48:31 -0400

    Set up html structure for base resume and mytimeline pages

--- front-end-angular/src/app/mytimeline/mytimeline.component.ts
+++ front-end-angular/src/app/mytimeline/mytimeline.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
+import { BaseNavComponent } from '../shared/shared-nav.component';
+import { BaseFooterComponent } from '../shared/shared-footer.component';
 
 @Component({
   selector: 'app-mytimeline',
   standalone: true,
-  imports: [],
-  template: `
-    <p>
-      mytimeline works!
-    </p>
-  `,
+  imports: [BaseNavComponent, BaseFooterComponent],
+  templateUrl: './mytimeline.component.html',
   styleUrl: './mytimeline.component.css'
 })
 export class MyTimelineComponent {
--- front-end-angular/src/app/resume/resume.component.ts
+++ front-end-angular/src/app/resume/resume.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
+import { BaseNavComponent } from '../shared/shared-nav.component';
+import { BaseFooterComponent } from '../shared/shared-footer.component';
 
 @Component({
   selector: 'app-resume',
   standalone: true,
-  imports: [],
-  template: `
-    <p>
-      resume works!
-    </p>
-  `,
+  imports: [BaseNavComponent, BaseFooterComponent],
+  templateUrl: './resume.component.html',
   styleUrl: './resume.component.css'
 })
 export class ResumeComponent {
