4f9a10d8287d6ce703ef719282298b24ebeb8122
reschmax <reschmax@msu.edu>
2024-07-25 11:34:56 -0400

    Fixed bug with overlapping templates

--- front-end-angular/src/app/app.component.ts
+++ front-end-angular/src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
+import { BaseComponent } from './shared/shared.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './home/home.component.html',
-  styleUrl: './app.component.css',
+  imports: [RouterOutlet, RouterLink, BaseComponent],
+  template: `
+    <app-home></app-home>
+  `,
+  // templateUrl: './shared/shared.component.html',
+  styleUrls: [
+    './app.component.css'
+  ],
 })
 export class AppComponent {
   title = 'front-end-angular';
--- front-end-angular/src/app/app.routes.ts
+++ front-end-angular/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { BaseComponent } from './shared/shared.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    //We will use the home page as the root of the application for now
+    {
+        path: '',
+        title: 'App Home Page',
+        component: AppComponent,
+    },
+    {
+        path: 'home',
+        title: 'App Home Page',
+        component: BaseComponent,
+    },
+];
