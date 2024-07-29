b06a49077c44e5b7666d32827da0d618e1dd3df0
reschmax <reschmax@msu.edu>
2024-07-29 12:13:19 -0400

    Completed Timeline page

--- front-end-angular/src/app/resume/resume.component.ts
+++ front-end-angular/src/app/resume/resume.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { BaseNavComponent } from '../shared/shared-nav.component';
 import { BaseFooterComponent } from '../shared/shared-footer.component';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-resume',
   standalone: true,
-  imports: [BaseNavComponent, BaseFooterComponent],
+  imports: [BaseNavComponent, BaseFooterComponent, NgOptimizedImage],
   templateUrl: './resume.component.html',
   styleUrl: './resume.component.css'
 })
