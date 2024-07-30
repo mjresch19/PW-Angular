4d2b11b577291f9596b1418556e2a33921f2d716
reschmax <reschmax@msu.edu>
2024-07-30 18:00:25 -0400

    Put the components within the correct directories. Timeline and resume were previously flipped

--- front-end-angular/src/app/mytimeline/mytimeline.component.ts
+++ front-end-angular/src/app/mytimeline/mytimeline.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { BaseNavComponent } from '../shared/shared-nav.component';
 import { BaseFooterComponent } from '../shared/shared-footer.component';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-mytimeline',
   standalone: true,
-  imports: [BaseNavComponent, BaseFooterComponent],
+  imports: [BaseNavComponent, BaseFooterComponent, NgOptimizedImage],
   templateUrl: './mytimeline.component.html',
   styleUrl: './mytimeline.component.css'
 })
