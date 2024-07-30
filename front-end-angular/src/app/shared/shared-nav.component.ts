59f77b6b3c3dde97108e1c03e7179b2de81fb35f
reschmax <reschmax@msu.edu>
2024-07-30 14:22:35 -0400

    Changed Nav-bar behavior

--- front-end-angular/src/app/shared/shared-nav.component.ts
+++ front-end-angular/src/app/shared/shared-nav.component.ts
@@ -1,46 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-shared-nav',
   standalone: true,
-  imports: [RouterLink, RouterOutlet],
+  imports: [RouterLink, RouterOutlet, NgOptimizedImage],
   template: `
     <nav>
-      <div class="flex-container">
-          <div id="sidebar" class="sidebar" aria-label="sidebar" aria-hidden="true">
-            <div class="sidebar_content">
-              <a routerLink="/home">Home Page</a>
-            </div>
-            <div class="sidebar_content">
-              <a routerLink="/projects">Project Page</a>
-            </div>
-            <div class="sidebar_content">
-              <a routerLink="/piano">Piano Page</a>
-            </div>
-            <div class="sidebar_content">
-              <a routerLink="/resume">Resume</a>
-            </div>
-            <div class="sidebar_content">
-              <a routerLink="/chat">Chat</a>
-            </div>
-            <div class="sidebar_content">
-            <a routerLink="/login">Login</a>
-            </div>
-            <div class="sidebar_content">
-              <a  target="_blank" href="https://www.linkedin.com/in/max-resch-22646219b/">LinkedIn</a>
-            </div>
-          </div>
-            <h1>Welcome to Max's Website!<span style="font-size: 16px"><br>CSE 477 - Homework 3</span></h1>
-          <!-- <button data-toggle-sidebar="sidebar"><img src="../assets/images/social-linkedin.png" alt="clover-icon" loading="lazy" class="icon" id="icon-object"></img></button> -->
-          <a routerLink="/home">Home Page</a>
-          <a routerLink="/projects">Projects Page</a>
-          <a routerLink="/piano">Piano Page</a>
-          <a routerLink="/resume">Resume</a>
-          <a routerLink="/chat">Chat</a>
-          <a routerLink="/login">Login</a>
-          <a target="_blank" href="https://www.linkedin.com/in/max-resch-22646219b/"><img src="../assets/images/social-linkedin.png" alt="URL to LinkedIn Page" loading="lazy"></a>
-      </div>
+      <ul class="link-group">
+        <li><a routerLink="/home">Home</a></li>
+        <li><a routerLink="/mytimeline">My Timeline</a></li>
+        <li><a routerLink="/resume">Resume</a></li>
+        <li><a routerLink="/login">Login</a></li>
+        <li><a routerLink="/tester">Become a Tester *Coming Soon*</a></li>
+        <li><a target="_blank" href="https://www.linkedin.com/in/max-resch-22646219b/"><img ngSrc="../assets/images/social-linkedin.png" alt="URL to LinkedIn Page" loading="lazy" width="40" height="40"></a></li>
+      </ul>
+
     </nav>
     <router-outlet></router-outlet>
   `,
@@ -49,3 +25,30 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 export class BaseNavComponent {
 
 }
+
+/*
+          <div id="sidebar" class="sidebar" aria-label="sidebar" aria-hidden="true">
+            <div class="sidebar_content">
+              <a routerLink="/home">Home</a>
+            </div>
+            <div class="sidebar_content">
+              <a routerLink="/mytimeline">My Timeline</a>
+            </div>
+            <div class="sidebar_content">
+              <a routerLink="/resume">Resume</a>
+            </div>
+            <div class="sidebar_content">
+              <a routerLink="null">Become a Tester *Coming Soon*</a>
+            </div>
+            <div class="sidebar_content">
+            <a routerLink="/login">Login</a>
+            </div>
+            <div class="sidebar_content">
+              <a  target="_blank" href="https://www.linkedin.com/in/max-resch-22646219b/">LinkedIn</a>
+            </div>
+          </div>
+            <!-- <h1>Welcome to Max's Website!</h1> -->
+          <!-- <button data-toggle-sidebar="sidebar"><img src="../assets/images/social-linkedin.png" alt="clover-icon" loading="lazy" class="icon" id="icon-object"></img></button> -->
+
+
+*/
